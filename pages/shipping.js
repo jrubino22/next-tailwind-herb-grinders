9dae49d90188a343fb2d2420a2d8d49fa264fe49
Joseph Rubino <joeyrubi22@gmail.com>
2022-10-27 15:55:53 -0500

    address form

---
+++ pages/shipping.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import CheckoutWizard from '../components/CheckoutWizard';
+import Layout from '../components/Layout';
+
+export default function ShippingScreen() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+    getValues,
+  } = useForm();
+  const submitHandler = ({
+    fullName,
+    address,
+    city,
+    postalCode,
+    country,
+  }) => {
+    type: 'SAVE_SHIPPING_ADDRESS',
+    payload: {fullName, address, city, postalCode, country, location}
+  };
+
+  return (
+    <Layout title="Shipping Address">
+      <CheckoutWizard activeStep={1} />
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Shipping Address</h1>
+        <div className="mb-4">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            className="w-full"
+            id="fullName"
+            autoFocus
+            {...register('FullName', {
+              required: 'Please enter full name',
+            })}
+          />
+          {errors.fullName && (
+            <div className="text-red-500">{errors.fullName.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="address">Address</label>
+          <input
+            className="w-full"
+            id="address"
+            {...register('address', {
+              required: 'Please enter address',
+              minLength: {
+                value: 3,
+                message: 'Address must be more than 2 chars',
+              },
+            })}
+          />
+          {errors.address && (
+            <div className="text-red-500">{errors.address.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">City</label>
+          <input
+            className="w-full"
+            id="city"
+            {...register('city', {
+              required: 'Please enter city',
+            })}
+          />
+          {errors.city && (
+            <div className="text-red-500">{errors.city.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            className="w-full"
+            id="postalCode"
+            {...register('postalCode', {
+              required: 'Please enter postal code',
+            })}
+          />
+          {errors.postalCode && (
+            <div className="text-red-500">{errors.postalCode.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="country">Country</label>
+          <input
+            className="w-full"
+            id="country"
+            {...register('country', {
+              required: 'Please enter country',
+            })}
+          />
+          {errors.country && (
+            <div className="text-red-500">{errors.country.message}</div>
+          )}
+        </div>
+        <div className="mb-4 flex justify-between">
+          <button className="primary-button">Next</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}

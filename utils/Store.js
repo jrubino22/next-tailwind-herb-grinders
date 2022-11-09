049e83392e996b22d03fa9ad1517305ad2532f49
Joseph Rubino <joeyrubi22@gmail.com>
2022-11-09 12:22:54 -0600

    fixed order issue

--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingAddress: {}, paymentMethod: '', },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
@@ -40,6 +40,8 @@ function reducer(state, action) {
           paymentMethod: '',
         },
       };
+    case 'CART_CLEAR_ITEMS':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
     case 'SAVE_SHIPPING_ADDRESS':
       return {
         ...state,

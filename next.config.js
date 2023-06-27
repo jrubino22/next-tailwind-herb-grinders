b0c778d08cfe13c55a4c27b8a13e0a1ceb2b89c1
Joseph Rubino <joeyrubi22@gmail.com>
2023-06-27 13:48:01 -0500

    add locale

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
   images: { domains: ['res.cloudinary.com', 'cdn.shopify.com'] },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
